b6269bec531a02f50eb4db51c98cb4de2169e314
PaoloTarcisio <paoloc50@gmail.com>
2024-01-17 13:23:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script >
+
+/*
+    1) Importo il componente
+        es. import *nome file* from *percorso file*
+
+    2) Dichiarazione del componente all'interno dell'oggetto
+        es. components:{
+            *nome file*
+        }
+
+    3) Aggiungo un tag con il nome del componente all'interno del template (template ha SOLO UN FIGLIO DIRETTO)
+*/ 
+
+import AppHeader from './components/AppHeader.vue'; // 1
+
+
+export default {
+  data() {
+    return { 
+      count: 0 
+    }
+  },
+  components: {             //2
+    AppHeader
+  }
+}
+</script>
+
+<template>
+
+    <div>
+        <AppHeader/>               
+        <h1>Hello World</h1>
+    
+        <p>
+            {{ count }}
+        </p>
+    
+        <button @click="count++">
+            Cliccami!
+        </button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+  data() {
+    return { 
+      message: 'Io Sono l\'Header, un componente aggiuntivo!' 
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>
+    {{ message }}
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
